1fc2c7aa827d9eb294b611b7758f028e8b59c90e
Valerio Martini <valerio.martini@gmail.com>
2024-06-26 13:26:46 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,38 @@
+
+
+tar -xvzf apstra-cloud-services-edge_4.2.1_0.0.36_1.tar.gz
+
+cd apstra-edge-0.0.36/
+
+vi docker-compose-0.0.36.yml
+
+# - REGISTRATION_KEY=<registration-code>
+# - CLOUD_TERM=ep-term.ai.juniper.net
+
+mkdir apstra_edge
+
+cp apstra-edge-0.0.36/docker-compose-0.0.36.yml apstra_edge/docker-compose.yml
+cp apstra-edge-0.0.36/apstra-edge-container-0.0.36.tgz apstra_edge/
+
+cd apstra_edge
+
+docker load < apstra-edge-container-0.0.36.tgz
+
+#
+# vi docker-compose.yml
+#- REGISTRATION_KEY=<registration-code>
+#- CLOUD_TERM=ep-term.ai.juniper.net
+# AOS_INSECURE_SKIP_VERIFY=true
+
+cd /etc/ssl/certs
+sudo cp ~/apstra-edge-0.0.36/ssl-keys/ep-term.ai.juniper.net.cer .
+
+sudo chmod 644 ep-term.ai.juniper.net.cer
+
+sudo update-ca-certificates
+
+cd apstra_edge
+docker compose up -d
+
+docker compose down
+docker compose up -d 
