c3f79d5ac8d847d1603b20c271c2aea3d28883ef
Valerio Martini <valerio.martini@gmail.com>
2024-06-26 17:43:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,28 +1,30 @@
 
+wget -O apstra-cloud-services-edge_4.2.1_0.0.36_1.tar.gz "https://JUNIPER-DOWLOAD-LINK.tar.gz"
+
 tar -xvzf apstra-cloud-services-edge_4.2.1_0.0.36_1.tar.gz
 
 cd apstra-edge-0.0.36/
 
-vi docker-compose-0.0.36.yml
+sudo vi docker-compose-0.0.36.yml
 
 mkdir apstra_edge
 
-cp apstra-edge-0.0.36/docker-compose-0.0.36.yml apstra_edge/docker-compose.yml
-cp apstra-edge-0.0.36/apstra-edge-container-0.0.36.tgz apstra_edge/
+sudo cp apstra-edge-0.0.36/docker-compose-0.0.36.yml apstra_edge/docker-compose.yml
+sudo cp apstra-edge-0.0.36/apstra-edge-container-0.0.36.tgz apstra_edge/
 
 cd apstra_edge
 
+read -p "Enter Your REGISTRATION_KEY: " KEY
+read -p "Enter Your Apstra_IP_Address: " IPA
+
 #
-sudo tee /apstra_edge/docker-compose.yml<<EOF
-example
-example test
+sudo tee /apstra_edge/docker-compose.yml <<EOF
+
+      - REGISTRATION_KEY=zgiDWJRKCcP0S8L06u5Gwby-NU1Lwu6Dyei62qnHx8jVYAAAj7Sgx2V6bv5KfwJ6
 EOF
 
 # - REGISTRATION_KEY=<registration-code>
 # - CLOUD_TERM=ep-term.ai.juniper.net
-
-# read -p "Enter Your REGISTRATION_KEY: " KEY
-# read -p "Enter Your Apstra_IP_Address: " IPA
 
 docker load < apstra-edge-container-0.0.36.tgz
 
